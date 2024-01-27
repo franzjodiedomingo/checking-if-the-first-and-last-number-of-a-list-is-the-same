47906563789340ae2200aa5c7f2eca7925aa7099
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-27 09:55:47 +0800

    final

--- checkingthesamedigits.py
+++ checkingthesamedigits.py
@@ -7,9 +7,10 @@ def check_first_and_last_equal(test_list):
         return False
     return test_list[0] == test_list[-1]
 
-test_list1 = [1,2,3,1]
-test_list2 = [1,2,3,4]
+test_list1 = [10,20,30,40,10]
+test_list2 = [75,65,35,75,30]
 
 print(f"Given List: {test_list1}")
 print("result is", check_first_and_last_equal(test_list1))
-print(f"numbers_y: {test_list2}, {check_first_and_last_equal(test_list2)}")
+print(f"numbers_y: {test_list2}")
+print("result is", check_first_and_last_equal(test_list2))
